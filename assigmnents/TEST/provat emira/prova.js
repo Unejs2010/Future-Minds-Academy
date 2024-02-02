9ecf56f7f617831ae34aadfc5a2344490f7db99a
Unejs2010 <unejsmurseli10@gmail.com>
2024-02-02 17:45:44 +0100

    yttthshsg

---
+++ assigmnents/TEST/provat emira/prova.js
@@ -0,0 +1,18 @@
+const $indicator = document.
+querySelector('.indicator');
+const $links = document.
+querySelectorAll('li a');
+
+$links.forEach(($links) => {
+$links.addEventListener(
+    'mousemove ',
+    (e) => {
+        const{ offsetLeft,
+            offsetWidth } = e.target;
+            $indicator.style.left =
+            offsetLeft + 'px';
+            $indicator.style.width =
+            offsetWidth + 'px';
+    }
+ );
+});
