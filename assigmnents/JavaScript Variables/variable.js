b23bb718e7b510caddb42a35327733b05faaf61d
Unejs2010 <unejsmurseli10@gmail.com>
2024-02-18 18:31:47 +0100

    hi

---
+++ assigmnents/JavaScript Variables/variable.js
@@ -0,0 +1,20 @@
+let age = 13;
+const yearOfBirth = 2010;
+
+const fingerPrint =33;
+
+console.log(age);
+
+console.log(yearOfBirth);
+
+
+// detyra tjeter \\
+
+
+let name ='Unejs' ;
+let mbiemri = 'Murseli';
+let mosha = 13;
+let datlindja = 2010;
+let musliman = true;
+console.log(name, mbiemri, mosha, datlindja,  musliman);
+
